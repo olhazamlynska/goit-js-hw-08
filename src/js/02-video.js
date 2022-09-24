968a4ecef8dc0c831f8c8bf6e843961a0f2e6608
olhazamlynska <olya.sonechko@gmail.com>
2022-09-24 20:24:59 +0300

    Update task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,15 +5,15 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
 
-console.log(KEY);
+console.log(STORAGE_KEY);
 
 const onTimeUpdate = function (event) {
   console.log(event.seconds);
-  localStorage.setItem('STORAGE_KEY', event.seconds);
+  localStorage.setItem(STORAGE_KEY, event.seconds);
 };
 player.on('timeupdate', throttle(onTimeUpdate, 1000));
 
-const onSaveCurrentTime = localStorage.getItem('STORAGE_KEY');
+const onSaveCurrentTime = localStorage.getItem(STORAGE_KEY);
 console.log(onSaveCurrentTime);
 
 if (onSaveCurrentTime) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,72 @@
+import throttle from 'lodash.throttle';
 
+const feedbackFormRef = document.querySelector('.feedback-form');
+
+const FORM_STORAGE_KEY = 'feedback-form-state';
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const remove = key => {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+inititPage();
+
+const throttledOnInputHandle = throttle(onInputHandle, 500);
+feedbackFormRef.addEventListener('input', throttledOnInputHandle);
+
+function onInputHandle(event) {
+  const { name, value } = event.target;
+  try {
+    let saveDataForm = load(FORM_STORAGE_KEY);
+    saveDataForm = saveDataForm ? saveDataForm : {};
+    saveDataForm[name] = value;
+    save(FORM_STORAGE_KEY, saveDataForm);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function inititPage() {
+  const saveDataForm = load(FORM_STORAGE_KEY);
+  console.log(saveDataForm);
+  if (!saveDataForm) {
+    return;
+  }
+  Object.entries(saveDataForm).forEach(([name, value]) => {
+    feedbackFormRef.elements[name].value = value;
+  });
+}
+
+const handleSubmit = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  remove(FORM_STORAGE_KEY);
+};
+
+feedbackFormRef.addEventListener('submit', handleSubmit);
