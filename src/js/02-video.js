ecd57f95dd51ee0f96a6641f7b431524133e5c37
olhazamlynska <olya.sonechko@gmail.com>
2022-09-24 15:39:22 +0300

    Update task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY = 'videoplayer-current-time';
+
+console.log(KEY);
+
+const onTimeUpdate = function (event) {
+  console.log(event.seconds);
+  localStorage.setItem('KEY', event.seconds);
+};
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+const onSaveCurrentTime = localStorage.getItem('KEY');
+console.log(onSaveCurrentTime);
+
+if (onSaveCurrentTime) {
+  player.setCurrentTime(onSaveCurrentTime);
+}
