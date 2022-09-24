07f10027a753d3d26adb60330e87c0246c70d7d0
olhazamlynska <olya.sonechko@gmail.com>
2022-09-24 12:15:47 +0300

    Task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryDivRef = document.querySelector('.gallery');
+
+const galleryImgItem = galleryItems
+  .map(({ preview, original, description }) => {
+    return /*html*/ `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join('');
+
+galleryDivRef.innerHTML = galleryImgItem;
+
+const modalGallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
